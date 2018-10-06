cbb37366f31c7e7a477a6029fd9714d02e149d84
NE0T12 <xgwtruth@outlook.com>
2018-10-06 10:25:16 +0800

    test

---
+++ test.cc
@@ -0,0 +1,14 @@
+ ///
+ /// @file    test.cc
+ /// @author  ne0t12
+ /// @date    2018-10-06 10:25:02
+ ///
+ 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <iostream>
+#include <string>
+using std::cout;
+using std::endl;
+using std::cin;
